8843e2df053e5d2095d93af1ca1dfa7fe6d8d4d7
johan1103 <johan1103@naver.com>
2023-07-05 01:34:59 +0900

    [Fix] release shell

--- release-process.sh
+++ release-process.sh
@@ -2,10 +2,11 @@
 
 sudo docker compose pull web1 -q
 sudo docker compose up --force-recreate -d web1
-curl --silent --head --fail https://goormtone6th.com/swagger-ui/index.html >> /dev/null
-while [ $? -ne 0 ];
-do 
-    sleep 1
-    curl --silent --head --fail https://goormtone6th.com/swagger-ui/index.html >> /dev/null
-done
+#curl --silent --head --fail https://goormtone6th.com/swagger-ui/index.html >> /dev/null
+#while [ $? -ne 0 ];
+#do 
+#    sleep 1
+#    curl --silent --head --fail https://goormtone6th.com/swagger-ui/index.html >> /dev/null
+#done
+
 echo -e "\033[32m"api-server 배포 성공"\033[0m" 
